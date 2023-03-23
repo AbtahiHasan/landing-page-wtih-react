66f76f9f05aede7fce735d7014bdb2b217208c31
AbtahiHasan <anikidpersonal@gmail.com>
2023-03-23 15:13:57 +0600

    update responsive

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -5,7 +5,7 @@ const Banner = () => {
     return (
         <section className='bg-black h-[90vh] flex justify-center items-center'>
             <div className='max-w-[1200px] mx-auto text-white font-bold grid md:grid-cols-2 gap-5'>
-                <div className='w-full mt-[30%]'>
+                <div className='w-full mt-[30%] px-5'>
                     <h3 className='text-base md:text-2xl'>Hello</h3>
                     <h1 className='text-2xl md:text-5xl md:mt-5'>Welcome</h1>
                     <h2 className='text-xl md:text-3xl md:mt-5'>Hi <Typed
@@ -18,7 +18,7 @@ const Banner = () => {
                              loop >
                          </Typed></h2> 
                 </div>
-                <div className='w-full'>
+                <div className='w-full px-5'>
                     <img src={logo} className='w-full h-full rounded-tl-[100px] rounded-br-[100px] border-4 border-sky-600 shadow-4xl' alt="anik" />
                 </div>
             </div>
